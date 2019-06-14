f5d0706048ea3ea30d9c32fa9ec26f746bf21cd6
Jonathan Thomas <jonathan.thomas251@gmail.com>
2019-06-14 13:50:28 -0400

    hello_world.java
    hello world in the Java programming language.....ye.

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+/* Java syntax for hello world program 
+This will print out the string "Hello world"
+*/
+
+public static void main(String [] args) {
+  System.out.println("Hello World!");
+  
+  }
+}
