483f01b4cf3fdec36c746295ff53363f0fccf58d
Jonathan Thomas <jonathan.thomas251@gmail.com>
2019-06-14 13:57:30 -0400

    hello.cpp
    Hello world program in C++ syntax

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //header file to be used by the compiler
+using namespace std;
+
+//main() is where the program execution begins
+int main() {
+  cout << "Hello World!" //prints out "Hello World!"
+  return 0;
+  
+}
+ 
