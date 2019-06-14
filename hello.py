3c14b6fdbf32bbf5060acd07f7713bc8031576aa
Jonathan Thomas <jonathan.thomas251@gmail.com>
2019-06-14 14:05:16 -0400

    hello.py
    Basic python3 syntax for "hello world"

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env_python3
+#syntax for python 3 as of 2019.....yeet!
+
+print "Hello, Python!"
+
+
+#to compile use the following commands in sucession from a command line interface
+#chmod +x hello.py
+#./hello.py
